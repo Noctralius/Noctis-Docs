5ecfc98fe6aeae4520f5d7f481d35435fcec1e1e
Noctralius <foxlite970@gmail.com>
2026-04-13 00:16:24 +0300

    Fix sync script types and separate build/sync steps

--- scripts/sync-content.ts
+++ scripts/sync-content.ts
@@ -6,15 +6,15 @@ const filePath = 'out/static.json';
 
 async function main() {
   const orama = new OramaCloud({
-    projectId: process.env.NEXT_PUBLIC_ORAMA_PROJECT_ID,
-    apiKey: process.env.ORAMA_PRIVATE_API_KEY,
+    projectId: process.env.NEXT_PUBLIC_ORAMA_PROJECT_ID!,
+    apiKey: process.env.ORAMA_PRIVATE_API_KEY!,
   });
 
   const content = await fs.readFile(filePath);
   const records = JSON.parse(content.toString()) as OramaDocument[];
 
   await sync(orama, {
-    index: process.env.NEXT_PUBLIC_ORAMA_DATASOURCE_ID,
+    index: process.env.NEXT_PUBLIC_ORAMA_DATASOURCE_ID!,
     documents: records,
   });
 
